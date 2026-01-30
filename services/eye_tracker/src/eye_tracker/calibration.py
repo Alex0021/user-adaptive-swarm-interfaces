453af1cff31696e7f2439b062b3946f99b60531c
AlexHeb-LIS <alexandre-1.hebert@outlook.com>
2026-01-30 13:22:38 +0100

    Added a monitor in the stream class to get more insights. Linter pass over code for formatting

--- services/eye_tracker/src/eye_tracker/calibration.py
+++ services/eye_tracker/src/eye_tracker/calibration.py
@@ -6,12 +6,18 @@ import logging
 from eye_tracker.constants import LOGS_DIR, DATA_DIR
 
 CALL_MODE = "usercalibration"
-SCREEN = "1"    
+SCREEN = "1"
 EXIT_ERROR_CALIBRATION_CANCELLED = 30
 
 logger = logging.getLogger(__name__)
 
-def invoke_eyetracker_calibration_manager(eyetracker):
+
+def invoke_eyetracker_calibration_manager(eyetracker: tr.EyeTracker | None):
+    """
+    Launch the Tobii Eye Tracker Manager for calibration. (External application)
+
+    :param eyetracker (tr.EyeTracker): The eye tracker device to calibrate. If None, the first available eye tracker will be used.
+    """
     if not eyetracker:
         logger.info("No eyetracker provided, using the first available eyetracker.")
         eyetrackers = tr.find_all_eyetrackers()
@@ -20,35 +26,50 @@ def invoke_eyetracker_calibration_manager(eyetracker):
             return
         eyetracker = eyetrackers[0]
 
-    logger.info("Starting eyetracker calibration with serial number %s on screen %s", eyetracker.serial_number, SCREEN)
+    logger.info(
+        "Starting eyetracker calibration with serial number %s on screen %s",
+        eyetracker.serial_number,
+        SCREEN,
+    )
     try:
-        etm_path = Path(os.environ['LOCALAPPDATA']) / "Programs/TobiiProEyeTrackerManager/TobiiProEyeTrackerManager.exe"
+        etm_path = (
+            Path(os.environ["LOCALAPPDATA"])
+            / "Programs/TobiiProEyeTrackerManager/TobiiProEyeTrackerManager.exe"
+        )
 
         log_file = open(LOGS_DIR / "etm_calibration.log", "w", encoding="utf-8")
 
-        etm_process = subprocess.Popen([
-            etm_path,
-            f"--mode={CALL_MODE}",
-            f"--device-sn={eyetracker.serial_number}",
-            f"--screen={SCREEN}"
-        ],
-        stdout=log_file)
+        etm_process = subprocess.Popen(
+            [
+                etm_path,
+                f"--mode={CALL_MODE}",
+                f"--device-sn={eyetracker.serial_number}",
+                f"--screen={SCREEN}",
+            ],
+            stdout=log_file,
+        )
 
         stdout, stderr = etm_process.communicate()
         log_file.close()
         if etm_process.returncode != 0:
-            for l in stdout.splitlines():
-                if l.startswith("ETM Error:"):
-                    logger.error(l)
+            for line in stdout.splitlines():
+                if line.startswith(bytes("ETM Error:", "utf-8")):
+                    logger.error(line)
         elif etm_process.returncode == EXIT_ERROR_CALIBRATION_CANCELLED:
             logger.warning("Calibration was cancelled by the user.")
         else:
             logger.info("Calibration exited successfully.")
 
     except Exception as e:
-        logger.error("An error occurred: %s", e)
+        logger.error("An error occurred: %s", e, exc_info=True)
+
 
 def save_latest_calibration(eyetracker: tr.EyeTracker):
+    """
+    Save the latest calibration data from the eye tracker to a file.
+
+    :param eyetracker (tr.EyeTracker): The eye tracker device from which to save calibration data.
+    """
     calib = eyetracker.retrieve_calibration_data()
     if not calib:
         logger.error("No calibration data found.")
@@ -60,7 +81,13 @@ def save_latest_calibration(eyetracker: tr.EyeTracker):
 
     logger.info("Calibration data saved to %s", file_path)
 
+
 def apply_saved_calibration(eyetracker: tr.EyeTracker):
+    """
+    Apply saved calibration data to the eye tracker.
+
+    :param eyetracker (tr.EyeTracker): The eye tracker device to which to apply calibration data.
+    """
     file_path = DATA_DIR / f"calibration_{eyetracker.serial_number}.bin"
     if not file_path.exists():
         logger.error("Calibration file %s does not exist.", file_path)
@@ -71,4 +98,3 @@ def apply_saved_calibration(eyetracker: tr.EyeTracker):
 
     eyetracker.apply_calibration_data(calib_data)
     logger.info("Applied calibration data from %s", file_path)
-
--- services/eye_tracker/src/eye_tracker/stream.py
+++ services/eye_tracker/src/eye_tracker/stream.py
@@ -4,36 +4,93 @@ import threading
 from collections import deque
 import logging
 
+
 class EyeTrackerStream:
     """
     Class to handle streaming data from the eye tracker using ZeroMQ.
     """
 
-    class Metrics:
+    class Monitor:
         """
         Used to get some insights on the streaming performance.
         """
+
         def __init__(self):
             self.total_messages = 0
+            self.interval_messages = 0
+            self.last_time = 0
+            self.queue_count = 0
+            self.estimated_frequency = 0.0
+            self.avg_queue_length = 0.0
+
+        def start(self):
+            self.reset()
+            self.last_time = time.time()
+
+        def reset(self):
+            self.total_messages = 0
+            self.last_time = 0
+            self.queue_count = 0
+            self.avg_queue_length = 0.0
+            self.estimated_frequency = 0.0
+
+        def update(self, msg_count: int, queue_count: int):
+            """
+            Update the monitor with new message and queue counts.
+
+            :param msg_count (int): Number of new messages sent.
+            :param queue_count (int): Number of messages currently in the queue.
+            """
+            self.total_messages += msg_count
+            self.interval_messages += msg_count
+            self.queue_count += queue_count
+            if time.time() - self.last_time >= 1.0:
+                self.estimated_frequency = self.interval_messages / (
+                    time.time() - self.last_time
+                )
+                self.avg_queue_length = (
+                    self.queue_count / self.total_messages
+                    if self.total_messages > 0
+                    else 0.0
+                )
+                self.last_time = time.time()
+                self.interval_messages = 0
+                self.queue_count = 0
 
         def get_frequency(self):
-            pass
+            """
+            Return the estimated frequency of messages being sent. (Updated every second)
+            """
+            return self.estimated_frequency
 
         def get_avg_queue_cnt(self):
-            pass
-            
+            """
+            Return the average queue length of messages.
+            """
+            return self.avg_queue_length
+
         def get_total_messages(self):
+            """
+            Return the total number of messages sent since the monitor started.
+            """
             return self.total_messages
 
-    def __init__(self, address="tcp://localhost", port=5555):
+    def __init__(self, address: str = "tcp://localhost", port: int = 5555):
+        """
+        Binds to the given address and port to stream eye tracker data.
+
+        :param address (str): The address to bind the stream to.
+        :param port (int): The port to bind the stream to.
+        """
         self.address = f"{address}:{port}"
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.PUB)
         self.socket.bind(self.address)
         self.running = False
-        self.msg_queue = deque(maxlen=100)
+        self.msg_queue: deque[dict[str, float]] = deque(maxlen=100)
         self.thread = None
         self._lock = threading.Lock()
+        self.monitor = EyeTrackerStream.Monitor()
         self.logger = logging.getLogger(__name__)
 
     def start_stream(self):
@@ -45,8 +102,9 @@ class EyeTrackerStream:
             self.running = True
             self.thread = threading.Thread(target=self._stream_data)
             self.thread.start()
+            self.monitor.start()
             self.logger.info("Eye tracker stream started.")
-    
+
     def stop_stream(self):
         """
         Stop the eye tracker data stream.
@@ -54,10 +112,11 @@ class EyeTrackerStream:
         if self.running:
             self.running = False
             self.thread.join()
+            self.monitor.reset()
             self.socket.close()
             self.context.term()
             self.logger.info("Eye tracker stream stopped.")
-    
+
     def _stream_data(self):
         """
         Internal method to simulate streaming data from the eye tracker.
@@ -67,9 +126,10 @@ class EyeTrackerStream:
                 with self._lock:
                     msg = self.msg_queue.popleft()
                 self.socket.send_json(msg)
+                self.monitor.update(1, len(self.msg_queue))
             time.sleep(0.01)  # Sleep briefly to avoid busy waiting
-    
-    def gaze_data_callback(self, gaze_data):
+
+    def gaze_data_callback(self, gaze_data: dict[str, float]):
         """
         Callback function to handle incoming gaze data from the eye tracker.
         This function should be connected to the eye tracker's data stream.
